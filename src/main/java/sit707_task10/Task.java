9a4dfbb30c5718e5dc4e6aea71306a976029b344
Bruce-Li7 <lijiaqi7788@gmail.com>
2024-05-27 18:10:56 +1000

    update complete task and task manager

--- src/main/java/sit707_task10/Task.java
+++ src/main/java/sit707_task10/Task.java
@@ -2,4 +2,15 @@ package sit707_task10;
 
 public class Task {
 
+	String name;
+    String studentId;
+    String email;
+    String comment;
+
+    public Task(String name, String studentId, String email, String comment) {
+        this.name = name;
+        this.studentId = studentId;
+        this.email = email;
+        this.comment = comment;
+    }
 }
--- src/main/java/sit707_task10/TaskManager.java
+++ src/main/java/sit707_task10/TaskManager.java
@@ -1,5 +1,26 @@
 package sit707_task10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
 
+	List<Task> inbox;
+
+    public TaskManager() {
+        this.inbox = new ArrayList<>();
+    }
+
+    public void submitTask(String name, String studentId, String email, String comment) {
+        Task task = new Task(name, studentId, email, comment);
+        this.inbox.add(task);
+    }
+
+    public void listTasks() {
+        for (Task task : this.inbox) {
+            System.out.println("Task name: " + task.name + ", ID: " + task.studentId + ", Email: " + task.email + ", Comment: " + task.comment);
+        }
+    }
+	
+	
 }
